3921c487f2f43d02c44edfd050573aa1c4036901
Mateo Encalada <mateo.encalada@udla.edu.ec>
2023-07-03 07:59:53 -0500

    Actualizacion carpeta Modelo e instalacion Nuggets Mvvm

--- Models/NotesME.cs
+++ Models/NotesME.cs
@@ -8,9 +8,56 @@ namespace ME_Taller1.Models
 {
     internal class NotesME
     {
-        //Prueba para git
+        
         public string Filename { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
+
+        public NotesME()
+        {
+            Filename = $"{Path.GetRandomFileName()}.notes.txt";
+            Date = DateTime.Now;
+            Text = "";
+        }
+
+        public void Save() =>
+    File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+
+        public void Delete() =>
+            File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename));
+
+        public static NotesME Load(string filename)
+        {
+            filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+
+            if (!File.Exists(filename))
+                throw new FileNotFoundException("Unable to find file on local storage.", filename);
+
+            return
+                new()
+                {
+                    Filename = Path.GetFileName(filename),
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetLastWriteTime(filename)
+                };
+        }
+        public static IEnumerable<NotesME> LoadAll()
+        {
+            // Get the folder where the notes are stored.
+            string appDataPath = FileSystem.AppDataDirectory;
+
+            // Use Linq extensions to load the *.notes.txt files.
+            return Directory
+
+                    // Select the file names from the directory
+                    .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                    // Each file name is used to load a note
+                    .Select(filename => NotesME.Load(Path.GetFileName(filename)))
+
+                    // With the final collection of notes, order them by date
+                    .OrderByDescending(note => note.Date);
+        }
     }
+    
 }
