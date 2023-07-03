c23cc4c01774fe3c8b3debeb2fb36bc5c05731e8
Mateo Encalada <mateo.encalada@udla.edu.ec>
2023-07-03 08:44:46 -0500

    Crear Modelo vista Notas

--- ViewModels/NotesViewModelME.cs
+++ ViewModels/NotesViewModelME.cs
@@ -1,12 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.Input;
+using ME_Taller1.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace ME_Taller1.ViewModels
 {
-    internal class NotesViewModelME
+    internal class NotesViewModelME: IQueryAttributable
     {
+        public ObservableCollection<ViewModels.NoteViewModelME> AllNotes { get; }
+        public ICommand NewCommand { get; }
+        public ICommand SelectNoteCommand { get; }
+
+        public NotesViewModelME()
+        {
+            AllNotes = new ObservableCollection<ViewModels.NoteViewModelME>(Models.NotesME.LoadAll().Select(n => new NoteViewModelME(n)));
+            NewCommand = new AsyncRelayCommand(NewNoteAsync);
+            SelectNoteCommand = new AsyncRelayCommand<ViewModels.NoteViewModelME>(SelectNoteAsync);
+        }
+
+        private async Task NewNoteAsync()
+        {
+            await Shell.Current.GoToAsync(nameof(Views.NotePageME));
+        }
+
+        private async Task SelectNoteAsync(ViewModels.NoteViewModelME note)
+        {
+            if (note != null)
+                await Shell.Current.GoToAsync($"{nameof(Views.NotePageME)}?load={note.Identifier}");
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("deleted"))
+            {
+                string noteId = query["deleted"].ToString();
+                NoteViewModelME matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note exists, delete it
+                if (matchedNote != null)
+                    AllNotes.Remove(matchedNote);
+            }
+            else if (query.ContainsKey("saved"))
+            {
+                string noteId = query["saved"].ToString();
+                NoteViewModelME matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note is found, update it
+                if (matchedNote != null)
+                    matchedNote.Reload();
+
+                // If note isn't found, it's new; add it.
+                else
+                    AllNotes.Add(new NoteViewModelME(NotesME.Load(noteId)));
+            }
+        }
     }
 }
